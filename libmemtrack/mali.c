21346ee23df9e7061ef803e343b08c9f1fd8d41a
Andreas Schneider <asn@cryptomilk.org>
2015-08-21 14:27:29 -0700

    libmemtrack: Update memtrack for newer Mali devices
    Change-Id: I75560c58ca2ec9febb361f608f693db2930aed2e

--- libmemtrack/mali.c
+++ libmemtrack/mali.c
@@ -17,11 +17,16 @@
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <dirent.h>
 #include <sys/mman.h>
 
 #include <hardware/memtrack.h>
 
 #include "memtrack_exynos5.h"
+
+/* Set a 512 MB limit */
+#define MAX_MEMTRACK_SIZE 0x20000000
 
 #define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
 #define min(x, y) ((x) < (y) ? (x) : (y))
@@ -44,9 +49,15 @@ int mali_memtrack_get_memory(pid_t pid, int type,
                              size_t *num_records)
 {
     size_t allocated_records = min(*num_records, ARRAY_SIZE(record_templates));
-    int i;
+    struct dirent *de;
+    DIR *dp;
     FILE *fp;
+    char pid_str[8];
+    char mali_file[32];
     char line[1024];
+    int len;
+    int total = 0;
+    int native_buffer = 0;
 
     *num_records = ARRAY_SIZE(record_templates);
 
@@ -55,7 +66,28 @@ int mali_memtrack_get_memory(pid_t pid, int type,
         return 0;
     }
 
-    fp = fopen("/sys/devices/platform/mali.0/gpu_memory", "r");
+    dp = opendir("/d/mali/mem/");
+    if (dp == NULL) {
+        return -errno;
+    }
+
+    len = snprintf(pid_str, sizeof(pid_str), "%d", pid);
+    if (len <= 0) {
+        closedir(dp);
+        return -EINVAL;
+    }
+
+    for (de = readdir(dp); de != NULL; de = readdir(dp)) {
+        if (strncmp(de->d_name, pid_str, len) != 0) {
+            continue;
+        }
+
+        snprintf(mali_file, sizeof(mali_file), "/d/mali/mem/%s", pid_str);
+        break;
+    }
+    closedir(dp);
+
+    fp = fopen(mali_file, "r");
     if (fp == NULL) {
         return -errno;
     }
@@ -64,33 +96,48 @@ int mali_memtrack_get_memory(pid_t pid, int type,
            sizeof(struct memtrack_record) * allocated_records);
 
     while (1) {
+        int rc;
+        char label1[16] = { 0 };
+        char label2[16] = { 0 };
+        unsigned int size = 0;
+
         if (fgets(line, sizeof(line), fp) == NULL) {
             break;
         }
 
         /* Format:
-         * Name                 pid  cap(pages) usage(pages) unmapped(pages)
-         * =================================================================
-         * mali0                        524288      85163
-         *   ndroid.calendar  10708    1048575        181        111
+         * TODO: list out format, use more descriptive names
          */
-        if (line[0] == ' ' && line[1] == ' ') {
-            unsigned int allocated;
-            unsigned int unmapped;
-            int line_pid;
+        rc = sscanf(line, "%s %*s %*s %u \n", label1, &size);
+        if (rc == 2) {
+            if (strcmp(label1, "Total") == 0) {
+                if (total + size > MAX_MEMTRACK_SIZE) {
+                    break;
+                }
+                total += size;
+            }
+            continue;
+        }
 
-            int ret = sscanf(line, "  %*s %u %*u %u %u\n",
-                             &line_pid, &allocated, &unmapped);
-            if (ret == 3 && line_pid == pid) {
-                if (allocated_records > 0) {
-                    records[0].size_in_bytes = (allocated - unmapped) * PAGE_SIZE;
+        /* Format:
+         * TODO: list out format, use more descriptive names
+         */
+        rc = sscanf(line, "%*s %s %s %*s %*s %u \n", label1, label2, &size);
+        if (rc == 3) {
+            if (strcmp(label1, "Native") == 0 &&
+                strcmp(label2, "Buffer") == 0) {
+                if (native_buffer + size > MAX_MEMTRACK_SIZE) {
+                    break;
                 }
-                if (allocated_records > 1) {
-                    records[1].size_in_bytes = unmapped * PAGE_SIZE;
-                }
-                break;
+                native_buffer += size;
             }
         }
+    }
+
+    records[0].size_in_bytes = native_buffer;
+
+    if (allocated_records == 2 && total >= native_buffer) {
+        records[1].size_in_bytes = total - native_buffer;
     }
 
     fclose(fp);
