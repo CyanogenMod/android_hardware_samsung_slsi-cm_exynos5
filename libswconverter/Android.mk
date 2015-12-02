6b464bdaa17a5fa507eebc66982a31217eaad713
Brandon McAnsh <brandon.mcansh@gmail.com>
2015-12-01 22:20:21 -0500

    libswconverter: Unoptimize for arm64
    * The assembly files are not 64-bit friendly
    
    Change-Id: I92c4e30df77d10584cc36c6865d0327c4749e2e9
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- libswconverter/Android.mk
+++ libswconverter/Android.mk
@@ -5,21 +5,23 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := \
-	swconvertor.c \
+	swconvertor.c
+
+# Only use NEON optimized assembly for arm targets
+LOCAL_SRC_FILES_arm += \
 	csc_tiled_to_linear_y_neon.s \
 	csc_tiled_to_linear_uv_neon.s \
 	csc_tiled_to_linear_uv_deinterleave_neon.s \
 	csc_interleave_memcpy_neon.s \
 	csc_ARGB8888_to_YUV420SP_NEON.s \
 	csc_ARGB8888_to_ABGR8888.s
+endif
 
 LOCAL_C_INCLUDES := \
 	$(TOP)/hardware/samsung_slsi-cm/openmax/include/khronos \
 	$(LOCAL_PATH)/../include
 
 LOCAL_MODULE := libswconverter
-
-LOCAL_MULTILIB := 32
 
 LOCAL_CFLAGS :=
 
