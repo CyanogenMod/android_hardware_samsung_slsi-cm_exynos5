d42717deaaa69f07a10383ac11c4f19d5e9f5617
Brandon McAnsh <brandon.mcansh@gmail.com>
2015-12-01 22:20:21 -0500

    libswconverter: Unoptimize for exynos7420
    * The assembly files are not 64-bit friendly
    
    Change-Id: I92c4e30df77d10584cc36c6865d0327c4749e2e9
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- libswconverter/Android.mk
+++ libswconverter/Android.mk
@@ -5,13 +5,17 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := \
-	swconvertor.c \
+	swconvertor.c
+
+ifneq ($(TARGET_SOC),exynos7420)
+LOCAL_SRC_FILES += \
 	csc_tiled_to_linear_y_neon.s \
 	csc_tiled_to_linear_uv_neon.s \
 	csc_tiled_to_linear_uv_deinterleave_neon.s \
 	csc_interleave_memcpy_neon.s \
 	csc_ARGB8888_to_YUV420SP_NEON.s \
 	csc_ARGB8888_to_ABGR8888.s
+endif
 
 LOCAL_C_INCLUDES := \
 	$(TOP)/hardware/samsung_slsi-cm/openmax/include/khronos \
